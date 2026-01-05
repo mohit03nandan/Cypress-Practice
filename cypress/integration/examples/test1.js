da689a139b0d0f60599f749b21c9cbc4e79aa5d7
mohit03nandan <mohitnandan81825@gmail.com>
2026-01-05 22:49:29 +0530

    added each function and parent child combination to find the locator

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -11,6 +11,21 @@ describe('My First Test', () => {
     cy.url().should('include', '/seleniumPractise')
     cy.get('.search-keyword').type('ca')
     cy.wait(2000)
-    cy.get('.product:visible').should('have.length',4);
+    // cy.get('.product:visible').should('have.length',4);
+    cy.get('.products').find('.product:visible').should('have.length',4);
+    cy.get('.products').find('.product:visible').eq(1).contains('ADD TO CART').click();
+    // parent child chaining
+    cy.get('.products').find('.product').should('have.length',4);
+    cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
+
+    // each function
+    cy.get('.products').find('.product').each(($el) => {
+      cy.wrap($el).find('h4.product-name').invoke('text').then((text) => {
+        if (text.includes('Brocoli')) {  
+          cy.wrap($el).find('button').click()
+        }
+      })
+    })
+
   })
 })
