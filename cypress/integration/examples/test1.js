5d524ba8164809360a70840762f2eb0be5eafc19
mohit03nandan <mohitnandan81825@gmail.com>
2026-01-06 23:04:15 +0530

    added alias , basic assertion and async behaviour code

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -1,31 +1,58 @@
-/// <reference types = "cypress" />
-// A solid test generally covers 3 phases:
+/// <reference types="Cypress" />
 
-// Set up the application state.
-// Take an action.
-// Make an assertion about the resulting application state.
+describe('My First Test Suite', function () {
 
-describe('My First Test', () => {
-  it('clicking "type" navigates to a new url', () => {
-    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
-    cy.url().should('include', '/seleniumPractise')
+  it('My FirstTest case', function () {
+
+
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
     cy.get('.search-keyword').type('ca')
     cy.wait(2000)
-    // cy.get('.product:visible').should('have.length',4);
-    cy.get('.products').find('.product:visible').should('have.length',4);
-    cy.get('.products').find('.product:visible').eq(1).contains('ADD TO CART').click();
-    // parent child chaining
-    cy.get('.products').find('.product').should('have.length',4);
-    cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
-
-    // each function
-    cy.get('.products').find('.product').each(($el) => {
-      cy.wrap($el).find('h4.product-name').invoke('text').then((text) => {
-        if (text.includes('Brocoli')) {  
-          cy.wrap($el).find('button').click()
-        }
-      })
+    //selenium get hit url in browser, cypress get acts like findElement of selenium
+    cy.get('.product').should('have.length', 5)
+    cy.get('.product:visible').should('have.length', 4)
+    //Parent child chaining
+    cy.get('.products').as('productLocator')
+    cy.get('@productLocator').find('.product').should('have.length', 4)
+    cy.get(':nth-child(3) > .product-action > button').click()
+    cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click().then(function () {
+      console.log('sf')
     })
 
+    cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+
+      const textVeg = $el.find('h4.product-name').text()
+      if (textVeg.includes('Cashews')) {
+        $el.find('button').click()
+      }
+    })
+
+    //assert if logo text is correctly displayed
+    cy.get('.brand').should('have.text', 'GREENKART')
+
+    //this is to print in logs
+    cy.get('.brand').then(function (logoelement) {
+      cy.log(logoelement.text())
+
+    })
+    //const logo=cy.get('.brand')
+    //cy.log(cy.get('.brand').text())
+    // cy.log(logo.text())
+
+
+
+
+
+
+
+
+
+
+
+    //fixture
+
   })
+
+
+
 })
